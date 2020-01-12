1aec29a9fd19ec08e0257734d2fa22aefa624215
HanpiJoker <2694238260@qq.com>
2020-01-12 10:14:18 +0800

    add get_ccl_cache.sh

--- nvim/get_ccl_cache.sh
+++ nvim/get_ccl_cache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ccls -index /home/spaceyu/Public/linux-4.9.99/ -init={"clang":{"excludeArgs":[
+ccls -index /home/spaceyu/Public/linux-4.9.99/ -init='{"clang":{"excludeArgs":[
 "-falign-jumps=1","-falign-loops=1","-fconserve-stack","-fmerge-constants","-fno-code-hoisting","-fno-schedule-insns","-fno-sched-pressure","-fno-var-tracking-assignments","-fsched-pressure",
 "-mhard-float","-mindirect-branch-register","-mindirect-branch=thunk-inline","-mpreferred-stack-boundary=2","-mpreferred-stack-boundary=3","-mpreferred-stack-boundary=4","-mrecord-mcount","-mindirect-branch=thunk-extern","-mno-fp-ret-in-387","-mskip-rax-setup",
 "--param=allow-store-data-races=0","-Wa,arch/x86/kernel/macros.s","-Wa,-"
-], "extraArgs":["--gcc-toolchain=aarch64-gnu-linux-gcc"]}}
+], "extraArgs":["--gcc-toolchain=aarch64-gnu-linux-gcc"]}}'
